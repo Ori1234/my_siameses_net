253c522190dd7523395e987f0edb8a7d015b6b89
oriterner <oriterner@gmail.com>
2016-11-15 15:15:34 +0200

    triple loss for my letters

---
+++ triplet-loss/data/data-test.lua
@@ -0,0 +1,70 @@
+torch.manualSeed(0)
+math.randomseed(0)
+
+data = require 'data'
+
+-- Shuffling training data
+data.select('train') -- or 'test'
+
+-- Initialise embeddings
+embDim =100 
+data.initEmbeddings(embDim)
+
+-- Get train and test number of batches
+batchDim = 50 
+trainBatches = data.getNbOfBatches(batchDim).train
+print('trainBatches: ', trainBatches)
+
+-- Get training batch nb 1
+batch = data.getBatch(1, 'train')
+print('batch = {aImg, pImg, nImg}: ', batch)
+i=math.random(batchDim);image.display(batch[1][i]); image.display(batch[2][i]); image.display(batch[3][i])
+-- Send batch to cuda
+collectgarbage()
+data.toCuda(batch)
+print('Batch sent to GPU memory')
+
+-- Saving embeddings
+data.saveEmb(torch.randn(batchDim, embDim), 1, 'train')
+print('Embedding saved for fast training')
+
+require '../model'
+
+opt={}
+batchNb=10
+opt.negPopMod='soft-neg'
+-- Snippet from the training script, provided for reference only
+do return end
+--[remove[
+if opt.negPopMode == 'soft-neg' then
+   -- Fetch triplet
+   x = data.getBatch(batchNb, 'train', 'soft-neg1')
+
+   if opt.cuda then data.toCuda(x) end
+   -- Update embeddings
+   local anchorsEmb = model.modules[1]:forward(x[1])
+   local positiveEmb = model.modules[2]:forward(x[2])
+   data.saveEmb(anchorsEmb, batchNb, 'train', positiveEmb)
+   -- Fetch new triplet
+   x1 = data.getBatch(batchNb, 'train', 'soft-neg2')
+elseif opt.negPopMode == 'hard-neg' then
+   -- Fetch triplet
+   x = data.getBatch(batchNb, 'train')
+   if opt.cuda then data.toCuda(x) end
+   -- Update embeddings
+   local anchorsEmb = model.modules[1]:forward(x[1])
+   data.saveEmb(anchorsEmb, batchNb, 'train')
+   -- Fetch new triplet
+   x = data.getBatch(batchNb, 'train')
+elseif opt.negPopMode == 'fast-hard-neg' then
+   -- Fetch triplet
+   x = data.getBatch(batchNb, 'train')
+end
+if opt.cuda then data.toCuda(x) end
+
+y = model:forward(x1)
+
+if opt.negPopMode == 'fast-hard-neg' then
+   data.saveEmb(y[1], batch, 'train')
+end
+--]]
